e9c8073172d18a22758ccb7660ffa7c5a9c5b13c
seantaylorlane <seantlane1999@gmail.com>
2017-04-22 19:26:23 -0400

    Revert from gradient

---
+++ change.js
@@ -0,0 +1,21 @@
+var changed = false;
+
+function change() {
+    var body = document.getElementsByTagName("body")[0];
+    var roaming = document.getElementById("roaming");
+    var artist1 = document.getElementById("artist1");
+    
+    if (changed){
+        body.style.background = "#7756eb";
+        artist1.style.opacity = "0";
+        roaming.style.opacity = "1";
+        changed = false;
+    } else {
+//        body.style.background = "#605f62";
+        body.style.background = "#5f528d";
+        roaming.style.opacity = "0";
+        artist1.style.opacity = "1";
+        changed = true;
+    }
+ 
+}
