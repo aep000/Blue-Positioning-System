4f0d33d4065c73e711bf98e805f0d32ad777093f
Alex Parson <alexparson1@gmail.com>
2017-04-22 14:31:36 -0400

    add backend

---
+++ backend/track.py
@@ -0,0 +1,20 @@
+import time
+import subprocess
+def getStrength(blueID="B8:53:AC:64:5E:59"):
+	bashCommand = "hcitool rssi "+blueID
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+	if(int(output.split(":")[1][1:])==0):
+		return 0
+	return int(output.split(":")[1][2:])
+'''
+while True:
+	val = getStrength()
+	if (val<20):
+		print "basement"
+	if (val>20 and val<30):
+		print "main"
+	if (val>30):
+		print "upstairs"
+	time.sleep(5)
+'''
