ec140309f342fd2e6e7d3a254e437a8f9ca7f2cf
seantaylorlane <seantlane1999@gmail.com>
2017-04-22 19:41:40 -0400

    Exhibit transition animation

--- change.js
+++ change.js
@@ -5,7 +5,7 @@ function change() {
     var roaming = document.getElementById("roaming");
     var artist1 = document.getElementById("artist1");
     
-    if (changed){
+   /* if (changed){
         body.style.background = "#7756eb";
         artist1.style.opacity = "0";
         roaming.style.opacity = "1";
@@ -15,6 +15,21 @@ function change() {
         body.style.background = "#5f528d";
         roaming.style.opacity = "0";
         artist1.style.opacity = "1";
+        changed = true;
+    }*/
+    
+     if (changed){
+        body.style.background = "#7756eb";
+        artist1.style.opacity = "0";
+        roaming.style.opacity = "1";
+        setTimeout(function() {artist1.style.display = "none";}, 750);
+        changed = false;
+    } else {
+//        body.style.background = "#605f62";
+        body.style.background = "#5f528d";
+        roaming.style.opacity = "0";
+        artist1.style.opacity = "1";
+        artist1.style.display = "block";
         changed = true;
     }
  
