7e0b751c11b7128323002eecc8f19536032e6a89
seantaylorlane <seantlane1999@gmail.com>
2017-04-23 02:08:50 -0400

    Demo cycle and function parameters

--- changeDemo.js
+++ changeDemo.js
@@ -1,15 +1,22 @@
-function change(exhibitNumber) {
+var changed = false;
+var exhibitInt = 1;
+
+function change() {
     var body = document.getElementsByTagName("body")[0];
     var roaming = document.getElementById("roaming");
-    var exhibitName = "exhibit"+exhibitNumber;
+    var exhibitName = "exhibit"+exhibitInt;
     var exhibit = document.getElementById(exhibitName);
     
-     if (exhibitNumber==0){
+     if (changed){
         body.style.background = "#7756eb";
         exhibit.style.opacity = "0";
         setTimeout(function() {exhibit.style.display = "none"; roaming.style.display = "flex";}, 500);
         setTimeout(function() {roaming.style.opacity = "1";}, 1000);
         changed = false;
+        exhibitInt++;
+         if (exhibitInt>3) {
+             exhibitInt = 1;
+         }
     } else {
 //        body.style.background = "#605f62";
         body.style.background = "#5f528d";
