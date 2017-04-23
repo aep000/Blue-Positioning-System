740e76ef878db3795e80d8ddb1b4c74587b154a9
seantaylorlane <seantlane1999@gmail.com>
2017-04-23 01:12:36 -0400

    Intro exhibit

--- backend/track.py
+++ backend/track.py
@@ -4,9 +4,11 @@ def getStrength(blueID="B8:53:AC:64:5E:59"):
 	bashCommand = "hcitool rssi "+blueID
 	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 	output, error = process.communicate()
+	print output
 	if(int(output.split(":")[1][1:])==0):
 		return 0
 	return int(output.split(":")[1][2:])
+getStrength()
 '''
 while True:
 	val = getStrength()
