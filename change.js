dc5098aa5d602c399af986f4dbf4a59fdc132fe5
seantaylorlane <seantlane1999@gmail.com>
2017-04-23 11:34:45 -0400

    AJAX change url fix

--- change.js
+++ change.js
@@ -1,40 +1,40 @@
-var location = "exhibit1";
-var currentLocation = "exhibit1";
+var eLocation = "exhibit1";
+var currenteLocation = "exhibit1";
 
 function change(exhibitName) {
     var body = document.getElementsByTagName("body")[0];
     var roaming = document.getElementById("roaming");
     var exhibit = document.getElementById(exhibitName);
-    var currentExhibit = document.getElementById(currentLocation);
-    console.log("Location is: " + location +" and the last location is " + currentLocation);
+    var currentExhibit = document.getElementById(currenteLocation);
+    console.log("Location is: " + eLocation +" and the last Location is " + currenteLocation);
     
-    if(location==currentLocation) {
+    if(eLocation==currenteLocation) {
         return;
-    } else if(location!=currentLocation) {
+    } else if(eLocation!=currenteLocation) {
         currentExhibit.style.opacity ="0";
         setTimeout(function() {
             currentExhibit.style.display = "none"; 
-            if (location=="exhibit1") {
+            if (eLocation=="exhibit1") {
                 exhibit.style.display = "flex";
             } else {
                 exhibit.style.display = "block";
             }
             currentExhibit.style.opacity = "1";
         }, 750);
-        currentLocation = location;
+        currenteLocation = eLocation;
     }
 }
     
-    function locationRequest() {
+    function eLocationRequest() {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        location = this.responseText;
+        eLocation = this.responseText;
         }
     };
     request.open("GET", "predict/", true);
     request.send();
-    change(location);
+    change(eLocation);
 }
 
-setInterval(locationRequest, 500);
+setInterval(eLocationRequest, 500);
