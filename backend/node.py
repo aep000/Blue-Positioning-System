7d284477af50cbcec4b8624a42ff8a9f0fe7dfff
seantaylorlane <seantlane1999@gmail.com>
2017-04-23 11:06:02 -0400

    AJAX

--- backend/node.py
+++ backend/node.py
@@ -2,8 +2,7 @@ import track
 import urllib2
 import time
 while True:
-	print "hi"
 	nodeId = 0
-	nodeUrl ="https://mysterious-lake-7602.herokuapp.com/updateNodeData?node="+str(nodeId)+"&rssi="+str(track.getStrength())
+	nodeUrl ="https://mysterious-lake-7602.herokuapp.com/updateNodeData?node="+str(nodeId)+"&rssi="+str(track.getStrength("00:34:DA:F4:02:6E"))
 	f = urllib2.urlopen(nodeUrl)
 	time.sleep(.5)
--- change.js
+++ change.js
@@ -1,10 +1,36 @@
-function change(exhibitNumber) {
+var location = "exhibit1";
+var currentLocation = "exhibit1";
+
+function locationRequest() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        location = this.responseText;
+        }
+    };
+    request.open("GET", "predict/", true);
+    request.send();
+    change(location);
+}
+
+function change(exhibitName) {
     var body = document.getElementsByTagName("body")[0];
     var roaming = document.getElementById("roaming");
-    var exhibitName = "exhibit"+exhibitNumber;
     var exhibit = document.getElementById(exhibitName);
+    var currentExhibit = document.getElementById(currentLocation);
+    console.log("Location is: " + location +" and the last location is " + currentLocation);
     
-     if (exhibitNumber==0){
+    if(location==currentLocation) {
+        return;
+    } else if(location!=currentLocation) {
+        currentExhibit.style.opacity ="0";
+        setTimeout(function() {currentExhibit.style.display = "none"; exhibit.style.display = "block"; currentExhibit.style.opacity = "1";}, 750);
+        currentLocation = location;
+    }
+    
+    
+    
+     /*if (exhibitNumber==0){
         body.style.background = "#7756eb";
         exhibit.style.opacity = "0";
         setTimeout(function() {exhibit.style.display = "none"; roaming.style.display = "flex";}, 500);
@@ -19,6 +45,6 @@ function change(exhibitNumber) {
         setTimeout(function() {roaming.style.display = "none";}, 750);
         changed = true;
         console.log(exhibitName);
-    }
+    }*/
  
 }
