6cfd2a8a6b39e35f706086393ccbdcabfd569089
seantaylorlane <seantlane1999@gmail.com>
2017-04-22 23:32:54 -0400

    Exhibit layout tweaks

--- change.js
+++ change.js
@@ -3,20 +3,20 @@ var changed = false;
 function change() {
     var body = document.getElementsByTagName("body")[0];
     var roaming = document.getElementById("roaming");
-    var exhibit1 = document.getElementById("exhibit1");
+    var exhibit = document.getElementById("exhibit1");
     
      if (changed){
         body.style.background = "#7756eb";
-        exhibit1.style.opacity = "0";
-        setTimeout(function() {exhibit1.style.display = "none"; roaming.style.display = "flex";}, 500);
+        exhibit.style.opacity = "0";
+        setTimeout(function() {exhibit.style.display = "none"; roaming.style.display = "flex";}, 500);
         setTimeout(function() {roaming.style.opacity = "1";}, 1000);
         changed = false;
     } else {
 //        body.style.background = "#605f62";
         body.style.background = "#5f528d";
         roaming.style.opacity = "0";
-        exhibit1.style.display = "block";
-        setTimeout(function() {exhibit1.style.opacity = "1"}, 500);
+        exhibit.style.display = "block";
+        setTimeout(function() {exhibit.style.opacity = "1"}, 500);
         setTimeout(function() {roaming.style.display = "none";}, 750);
         changed = true;
     }
