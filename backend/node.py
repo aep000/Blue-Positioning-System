2c8a3b5dacd4031ab489d4f3067fcf422eb6abb1
Alex Parson <alexparson1@gmail.com>
2017-04-23 11:19:17 -0400

    change

--- backend/node.py
+++ backend/node.py
@@ -2,8 +2,10 @@ import track
 import urllib2
 import time
 while True:
-	print "hi"
-	nodeId = 0
-	nodeUrl ="https://mysterious-lake-7602.herokuapp.com/updateNodeData?node="+str(nodeId)+"&rssi="+str(track.getStrength())
-	f = urllib2.urlopen(nodeUrl)
-	time.sleep(.5)
+	try:
+		nodeId = 0
+		nodeUrl ="https://mysterious-lake-7602.herokuapp.com/updateNodeData?node="+str(nodeId)+"&rssi="+str(track.getStrength())
+		f = urllib2.urlopen(nodeUrl)
+		time.sleep(.5)
+	except:
+		pass
